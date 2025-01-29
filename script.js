e989b3fdf983a1751d12233e98d6369d6aad9db4
Gana Syam Reddy <76063463+syam888@users.noreply.github.com>
2025-01-29 06:52:15 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,32 @@
+// Typing Animation
+const roles = ["Data Scientist", "Software Engineer", "Cloud Enthusiast"];
+let roleIndex = 0;
+let charIndex = 0;
+const typingText = document.querySelector(".typing-text");
+
+function type() {
+  if (charIndex < roles[roleIndex].length) {
+    typingText.textContent += roles[roleIndex].charAt(charIndex);
+    charIndex++;
+    setTimeout(type, 100);
+  } else {
+    setTimeout(erase, 2000);
+  }
+}
+
+function erase() {
+  if (charIndex > 0) {
+    typingText.textContent = roles[roleIndex].substring(0, charIndex - 1);
+    charIndex--;
+    setTimeout(erase, 50);
+  } else {
+    roleIndex = (roleIndex + 1) % roles.length;
+    setTimeout(type, 500);
+  }
+}
+
+type();
+
 // Form Submission Handling
 document.getElementById('contact-form').addEventListener('submit', function (e) {
   e.preventDefault();
